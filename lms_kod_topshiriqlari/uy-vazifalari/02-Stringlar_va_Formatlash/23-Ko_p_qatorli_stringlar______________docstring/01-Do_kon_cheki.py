e94deec3aabce739b9c0eacf4589dffd20977282
fozilovotir-hash <fozilovotir@gmail.com>
2026-07-11 23:46:59 +1000

    Add: Do'kon cheki [fozilovotir-hash]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/23-Ko_p_qatorli_stringlar______________docstring/01-Do_kon_cheki.py
@@ -0,0 +1,11 @@
+mahsulot = input()
+narx = int(input())
+soni = int(input())
+jami = narx * soni
+chek = f"""===== CHEK =====
+Mahsulot: {mahsulot}
+Narx: {narx}
+Soni: {soni}
+Jami: {jami}
+================"""
+print(chek)
