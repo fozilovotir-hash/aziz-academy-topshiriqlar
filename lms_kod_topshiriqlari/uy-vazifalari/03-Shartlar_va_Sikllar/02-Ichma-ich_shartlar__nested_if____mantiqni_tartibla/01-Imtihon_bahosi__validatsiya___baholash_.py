17063ce3e63309b9521dfb4831a8bd2fdeed25d1
fozilovotir-hash <fozilovotir@gmail.com>
2026-07-16 23:20:50 +1000

    Add: Imtihon bahosi (validatsiya + baholash) [fozilovotir-hash]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Imtihon_bahosi__validatsiya___baholash_.py
@@ -0,0 +1,12 @@
+ball = int(input())
+if 0 <= ball <= 100:
+    if ball >= 90:
+        print("Alo")
+    elif ball >= 75:
+        print("Yaxshi")
+    elif ball >= 60:
+        print("Qoniqarli")
+    else:
+        print("Yiqildi")
+else:
+    print("Notogri ball")    
