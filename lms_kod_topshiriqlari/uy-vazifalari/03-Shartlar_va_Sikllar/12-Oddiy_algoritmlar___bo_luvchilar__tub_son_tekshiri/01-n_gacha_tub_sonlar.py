858ffdc961021e228984383b6ff5e6a28889a757
fozilovotir-hash <fozilovotir@gmail.com>
2026-09-15 23:03:34 +1000

    Add: n gacha tub sonlar [fozilovotir-hash]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/12-Oddiy_algoritmlar___bo_luvchilar__tub_son_tekshiri/01-n_gacha_tub_sonlar.py
@@ -0,0 +1,11 @@
+n = int(input())
+primes = []
+for num in range(2, n + 1):
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(str(num))
+print(" " .join(primes))
